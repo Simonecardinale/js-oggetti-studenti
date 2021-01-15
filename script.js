579909f8fe370b8ef6c631224af2c63f106ec98f
Simone Cardinale <simonecardinale8@gmail.com>
2021-01-15 15:46:18 +0100

    work complete + "for cycle" version
    Add for cycle to exercise's third point

--- script.js
+++ script.js
@@ -31,18 +31,36 @@ for (var j in studentiArray){
 }
 
 
-var utenteNome = prompt("Inserisci nome");
-var utenteCognome = prompt("Inserisci cognome");
-var utenteEta = parseInt(prompt("Inserisci età"));
-var obj = { 
-            nome:  utenteNome, 
-            cognome:  utenteCognome, 
-            eta:  utenteEta 
-        };
+// var utenteNome = prompt("Inserisci nome");
+// var utenteCognome = prompt("Inserisci cognome");
+// var utenteEta = parseInt(prompt("Inserisci età"));
+// var obj = { 
+//             nome:  utenteNome, 
+//             cognome:  utenteCognome, 
+//             eta:  utenteEta 
+//         };
 
-studentiArray.push(obj);
+// studentiArray.push(obj);
 
 
+// for (var i in studentiArray){
+//     console.log(studentiArray[i]);
+// }
+
+var arrayObj = ["nome", "cognome", "età"];
+var arrayPrompt = [];
+for (var h = 0; h < arrayObj.length; h++){
+    var utente = prompt("Inserisci" + " " + arrayObj[h]);
+    arrayPrompt.push(utente);
+}
+
+var obj2 = {
+    nome: arrayPrompt[0],
+    cognome: arrayPrompt[1],
+    eta: arrayPrompt[2]
+};
+
+studentiArray.push(obj2);
 for (var i in studentiArray){
     console.log(studentiArray[i]);
 }
